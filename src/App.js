875799e3f6e56986e554d5a2797b324be4305a2a
EthanAndreas <ethanhuret27@gmail.com>
2023-03-29 17:49:15 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import axios from "axios";
 const App = () => {
     const [data, setData] = useState([]);
 
-    // Ajouter le titre du projet
-    const title = "Mon projet";
+    const title = "Retrieve the data on unknown IP address and print it with React";
 
     useEffect(() => {
         const fetchData = async () => {
             const validIPs = [];
 
-            // Vérifier les adresses IP valides
-            for (let i = 10; i <= 254; i++) {
+            for (let i = 5; i <= 254; i++) {
                 const ip = `10.0.0.${i}`;
                 try {
                     await axios.get(`http://${ip}`);
                     validIPs.push(ip);
                 } catch (error) {
-                    console.log(`L'adresse IP ${ip} est invalide.`);
+                    console.log(`Invalip IP address ${ip}.`);
                 }
             }
 
-            // Récupérer les données des adresses IP valides
             const requests = validIPs.map((ip) => axios.get(`http://${ip}`));
             const responses = await Promise.all(requests);
             const data = responses.map((response) => response.data);
@@ -34,7 +31,6 @@ const App = () => {
 
     return (
         <div>
-            {/* Afficher le titre du projet */}
             <h1>{title}</h1>
             <ul>
                 {data.map((item, index) => (

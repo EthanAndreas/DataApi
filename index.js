b1f0f47ae56ab617c1f86a2d2dcfea41c9a2a0b8
Your Name <you@example.com>
2023-03-30 10:13:36 +0200

    title

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const coap = require('coap');
 const express = require('express');
 const app = express();
 
-// Endpoint to handle CoAP requests and display the response on a webpage
+// Define the style and background image constants
+const style = 'style="width: 100%; font-family: "Gill Sans"; color:rgb(0,153,153); text-align: left;';
+
 app.get('/', (req, res) => {
-    // Set the title of the webpage
-    res.write('<html><head><title>Retrieve the data on unknown IP address and print it with NodeJS</title></head><body>');
+
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>Data Retrieve API</title>');
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<div>');
+    res.write('<h1> Retrieve the data on unknown IP address and print it with NodeJS</h1>');
+    res.write('</div>');
+    res.write('<div>');
+
+
+    console.log('Request received');
 
     // Iterate through IP addresses between 10.0.0.5 and 10.0.0.254
     for (let i = 5; i <= 254; i++) {
@@ -20,19 +34,26 @@ app.get('/', (req, res) => {
             coapRes.on('end', () => {
                 console.log(`Response from ${ip}: ${data}`);
                 // Display the response on the webpage
-                res.write(`<div style="margin-bottom: 10px;"><h2>Response from ${ip}:</h2><p>${data}</p></div>`);
+                res.write(`<div ${style} style="margin-bottom: 10px;"><h2>Response from ${ip}:</h2><p>${data}</p></div>`);
             });
         }).end();
     }
 
-    // Close the HTML tags
-    res.write('</body></html>');
+    // Close the container div and the HTML tags
+    res.write('</div>');
+    res.write('</body>');
+    res.write('</html>');
 
     // Send a response to the client
     res.end();
 });
 
+
+// Serve the background image
+app.use('/assets', express.static('assets'));
+
 // Start the server
 app.listen(3000, () => {
+
     console.log('Server started on port 3000');
 });

92e19d9b958032d8b072211988758fee99b4c8d1
EthanAndreas <ethanhuret27@gmail.com>
2023-03-31 19:07:05 +0200

    simulation part

--- app.py
+++ app.py
@@ -5,6 +5,47 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+import socket
+import subprocess
+
+def request_ip_addr(ip_addr):
+    
+    # Test if IP address is empty
+    if ip_addr == '':
+        return -2
+    
+    # Test IP address format 
+    parts = ip_addr.split('.')
+    if len(parts) != 4:
+        return -1
+    for part in parts:
+        if not part.isdigit():
+            return -1
+        num = int(part)
+        if num < 0 or num > 255:
+            return -1
+
+    # Validate the IP address before running the command
+    try:
+        socket.inet_aton(ip_addr)
+    except socket.error:
+        return -1
+
+    # Check if the IP address is reachable
+    response = subprocess.run(['ping', '-c', '0.7', ip_addr], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if response.returncode != 0:
+        return -1
+
+    # Run the shell command and capture the output
+    command = 'coap-client -m get "coap://{}/temperature/meas1"'.format(ip_addr)
+    try:
+        result = subprocess.check_output(command, shell=True)
+    except subprocess.CalledProcessError as e:
+        return -1
+
+    # Output the result
+    return result.decode()
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -13,44 +54,40 @@ def home():
 def recovery():
     if request.method == 'POST':
         ip_addr = request.form['ip_addr']
+        result = request_ip_addr(ip_addr)
 
-        # Validate the IP address before running the command
-        try:
-            socket.inet_aton(ip_addr)
-        except socket.error:
-            return 'Invalid IP address'
+        return render_template('recovery.html', result=result, ip_addr=ip_addr)
 
-        # Check if the IP address is reachable
-        response = subprocess.run(['ping', '-c', '0.5', ip_addr], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if response.returncode != 0:
-            return render_template('error.html', ip_addr=ip_addr)
-
-        # Run the shell command and capture the output
-        command = 'coap-client -m get "coap://{}/temperature/meas1"'.format(ip_addr)
-        try:
-            result = subprocess.check_output(command, shell=True)
-        except subprocess.CalledProcessError as e:
-            return 'Error running command: {}'.format(str(e))
-
-        # Output the result and add another input field and button for selecting another IP address
-        return render_template('recovery.html', result=result.decode(), ip_addr=ip_addr)
-
-    # If no form has been submitted yet, display the form
     return render_template('recovery.html')
-
-@app.route('/error')
-def error():
-    ip_addr = request.args.get('ip_addr')
-    return render_template('error.html', ip_addr=ip_addr)
 
 @app.route('/simulation', methods=['GET', 'POST'])
 def simulation():
     
     if request.method == 'POST':
-        ip_addr = request.form['ip_addr']
+        num_addr = int(request.form['num_addr'])
+        ip_addr = []
+        result = []
+        
+        if num_addr == 1:
+            ip_addr.append(request.form['ip_addr_1'])
+            result.append(request_ip_addr(ip_addr[0]))
+            
+        elif num_addr == 2:
+            ip_addr.append(request.form['ip_addr_1'])
+            ip_addr.append(request.form['ip_addr_2'])
+            result.append(request_ip_addr(ip_addr[0]))
+            result.append(request_ip_addr(ip_addr[1]))
+            
+        else:
+            ip_addr.append(request.form['ip_addr_1'])
+            ip_addr.append(request.form['ip_addr_2'])
+            ip_addr.append(request.form['ip_addr_3'])
+            result.append(request_ip_addr(ip_addr[0]))
+            result.append(request_ip_addr(ip_addr[1]))
+            result.append(request_ip_addr(ip_addr[2]))
+            
+        return render_template('simulation.html', ip_addr=ip_addr, result=result, num_addr=num_addr)
     
-
-    # Output the results
     return render_template('simulation.html')
 
 
